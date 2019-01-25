cb9c4053773fda7385d7f9808f75223fc0cdea39
Pablo Vent <ventpdev@gmail.com>
2019-01-25 22:30:04 +0000

    exercise 3 completed

---
+++ chap1basics/exer3.rb
@@ -0,0 +1,18 @@
+movies = {
+  :gone_with_the_wind => '1948',
+  :last_of_mohicans => '1996',
+  :mystic_river => '1992',
+  :forrest_gump => '1990',
+  :good_year => '2002'
+}
+
+# movies.each do |title, year|
+#   puts year
+# end
+
+puts movies[:gone_with_the_wind]
+puts movies[:last_of_mohicans]
+puts movies[:mystic_river]
+puts movies[:forrest_gump]
+puts movies[:good_year]
+
