348d7a3aa1c2efa11327c2c9a737d7966bff741e
Pablo Vent <ventpdev@gmail.com>
2019-01-26 17:17:23 +0000

    exercise 5 completed

---
+++ chap1basics/exer5.rb
@@ -0,0 +1,6 @@
+five_factorial = 5 * 4 * 3 * 2 * 1
+six_factorial = 6 * 5 * 4 * 3 * 2 * 1
+seven_factorial = 7 * 6 * 5 * 4 * 3 * 2 * 1
+eight_factorial = 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts " 5! = #{five_factorial}, 6! = #{six_factorial}, 7! = #{seven_factorial} and 8! = #{eight_factorial}"
