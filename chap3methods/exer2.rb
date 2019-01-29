b4c50f37965d6a251f712996f90cf36e59c29f38
Pablo Vent <ventpdev@gmail.com>
2019-01-29 23:52:58 +0000

    exercise 2 chap 3 completed

---
+++ chap3methods/exer2.rb
@@ -0,0 +1,5 @@
+x = 2 # returns 2
+puts x = 2 # prints 2 and returns nil
+p name = "Joe" # prints "Joe" and returns "Joe"
+four = "four" # returns "four"
+print something = "nothing" # prints "nothing" and returns nil on the same line.
