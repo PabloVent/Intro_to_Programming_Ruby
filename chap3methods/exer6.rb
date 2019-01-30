d7e0055193e2e3a34dfa6c861aea5dbd812b391f
Pablo Vent <ventpdev@gmail.com>
2019-01-30 18:31:33 +0000

    chap 3 exercise 6 completed

---
+++ chap3methods/exer6.rb
@@ -0,0 +1,12 @@
+# ArgumentError: wrong number of arguments (1 for 2)
+#   from (irb):1:in `calculate_product'
+#   from (irb):4
+#   from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
+
+# This error tells me that we're passing just one arguments, when it expects two instead.  See example below.
+
+def multiply(a, b)
+  a * b
+end
+
+puts multiply(a)
