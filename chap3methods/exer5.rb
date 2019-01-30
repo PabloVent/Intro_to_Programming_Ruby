ef47d01ca16e8f905fc06de28cc6817f8fde5fa7
Pablo Vent <ventpdev@gmail.com>
2019-01-30 18:37:30 +0000

    chap 3 exercise 5 completed

--- chap3methods/exer5.rb
+++ chap3methods/exer5.rb
@@ -6,5 +6,5 @@ end
 
 p scream("Yippeee")
 
-# By comenting out the explicit return, the method returns nil, since the method 'puts' returns nil when it's the last line to execute in a method and there's no explicit returns before it.
+# By comenting out the explicit return or deleting it, the method returns nil, since the method 'puts' returns nil when it's the last line to execute in a method and there's no explicit returns before it.
 
