7c4e7c2dcd7f71de023e1fb6e5344ee8dea85ad1
Pablo Vent <ventpdev@gmail.com>
2019-01-31 18:03:47 +0000

    chap 4 exercise 3 completed

--- chap4_flowControl/exer3.rb
+++ chap4_flowControl/exer3.rb
@@ -1,13 +1,13 @@
 puts "type a number between 0 and 100"
 number_input = gets.chomp.to_i
 
-if number_input >= 0 && number_input <= 50
-  puts "Your number lies between 0 and 50"
-elsif number_input < 0
+if number_input < 0
   puts "It can't be a negative number"
+elsif number_input >= 0 && number_input <= 50 
+  puts "Your number lies between 0 and 50" 
 elsif number_input > 50 && number_input <= 100
   puts "Your number lies between 51 and 100"
 else number_input > 100
-  puts "less than or equal to 100"
+  puts "It has to be less than or equal to 100"
 end
   
