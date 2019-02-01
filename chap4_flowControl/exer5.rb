d0b51e6edf47cf22dabd35ee3d88828b5263179d
Pablo Vent <ventpdev@gmail.com>
2019-02-01 08:35:20 +0000

    chap 4 exercise 5 completed

--- chap4_flowControl/exer5.rb
+++ chap4_flowControl/exer5.rb
@@ -16,3 +16,19 @@ def num_choose(number_input)
 end
 
 num_choose(number_input)
+
+# def num_choose(number_input)
+# case
+
+#   when number_input >= 0 && number_input <= 50 
+#     puts "Your number lies between 0 and 50" 
+#   when number_input > 50 && number_input <= 100
+#     puts "Your number lies between 51 and 100"
+#   when number_input < 0
+#     puts "It can't be a negative number"
+#   else number_input > 100
+#     puts "It has to be less than or equal to 100"
+#   end
+# end
+
+# num_choose(number_input)
