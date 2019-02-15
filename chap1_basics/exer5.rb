aa4262ad53e32091f1e1f14234fe77fe93e39829
Pablo Vent <ventpdev@gmail.com>
2019-02-15 20:20:19 +0000

    chap 1 exercise 5 completed

--- chap1_basics/exer5.rb
+++ chap1_basics/exer5.rb
@@ -1,6 +1,26 @@
-five_factorial = 5 * 4 * 3 * 2 * 1
-six_factorial = 6 * 5 * 4 * 3 * 2 * 1
-seven_factorial = 7 * 6 * 5 * 4 * 3 * 2 * 1
-eight_factorial = 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts "choose a positive number"
+num = gets.chomp.to_i
 
-puts " 5! = #{five_factorial}, 6! = #{six_factorial}, 7! = #{seven_factorial} and 8! = #{eight_factorial}"
+if num == 5
+factorial = num * (num - 1) * (num - 2) * (num - 3)
+puts "The factorial of #{num} is #{factorial}"
+elsif num == 6
+factorial = num * (num - 1) * (num - 2) * (num - 3)* (num - 4)
+puts "The factorial of #{num} is #{factorial}"
+elsif num == 7    
+factorial = num * (num - 1) * (num - 2) * (num - 3) * (num - 4) * (num - 5)
+puts "The factorial of #{num} is #{factorial}"
+elsif num == 8
+factorial = num * (num - 1) * (num - 2) * (num - 3) * (num - 4) * (num - 5)* (num - 6)
+puts "The factorial of #{num} is #{factorial}"
+else
+  puts "Your number wasn't either 5, 6, 7, 8."
+end
+
+
+# five_factorial = 5 * 4 * 3 * 2 * 1
+# six_factorial = 6 * 5 * 4 * 3 * 2 * 1
+# seven_factorial = 7 * 6 * 5 * 4 * 3 * 2 * 1
+# eight_factorial = 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+# puts " 5! = #{five_factorial}, 6! = #{six_factorial}, 7! = #{seven_factorial} and 8! = #{eight_factorial}"
