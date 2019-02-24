6773ecfbab6a188521a1e0bffdcd22137215965b
Pablo Vent <ventpdev@gmail.com>
2019-02-24 17:46:34 +0000

    chap 6 exercise 7 completed

--- chap6_arrays/exer7.rb
+++ chap6_arrays/exer7.rb
@@ -3,3 +3,15 @@ p test_array = [2, 3, 4, 5]
 
 p mapped_array = test_array.map { |elem| elem + 2 }
 #p mapped_array
+
+# or 
+
+arr = [1, 3, 5, 7, 9, 11]
+arr2 = []
+
+p arr
+arr.each do |e|
+  e += 2
+  arr2.push(e)
+end
+p arr2
