f64ab01465114941e8786e67a5482eba4b6b36ad
Pablo Vent <ventpdev@gmail.com>
2019-03-04 18:25:43 +0000

    Chap 8 exercise 3 completed

---
+++ chap8_more_stuff/exer3.rb
@@ -0,0 +1,4 @@
+=begin
+Exception handling is the way errors are managed. 
+By using begin, rescue, end, we anticipate and deal effectively with any possible scenarios whereby an error condition might arise or a bug introduced which otherwise might interrupt the normal flow of our program and stop it altogether. With error handling we trap/isolate the error while allowing for the rest of the program to flow normally.
+=end
