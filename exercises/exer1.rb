6cb1dc89676072486c7fd70981516c46341a312d
Pablo Vent <ventpdev@gmail.com>
2019-03-05 18:58:07 +0000

    end of book exercises 1 and 2 completed

---
+++ exercises/exer1.rb
@@ -0,0 +1,5 @@
+arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+arr1.each do |elem|
+  puts elem
+end
---
+++ exercises/exer2.rb
@@ -0,0 +1,7 @@
+arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+arr1.each do |elem|
+  if elem > 5
+  puts elem
+  end
+end
