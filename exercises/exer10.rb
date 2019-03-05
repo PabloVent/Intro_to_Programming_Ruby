59ba3197dd75a436dd659d51dd754d70dfa15f57
Pablo Vent <ventpdev@gmail.com>
2019-03-05 21:27:56 +0000

    end of book exercise 10 completed

---
+++ exercises/exer10.rb
@@ -0,0 +1,5 @@
+# Can hash values be arrays? YES.
+p person = { full_name: ['Paul', 'Ringwood'], age: 45}
+
+# Can you have an array of hashes? Absolutely!!
+p my_arr = [{ name: 'Paul', age: 45, eyes: 'Brown'}, { city: 'London', address: '12 main rd', County:'Hampshire'}]
