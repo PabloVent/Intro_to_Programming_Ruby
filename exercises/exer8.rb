38d226eadba9f894b5796323ec670a6f6fe7c8ea
Pablo Vent <ventpdev@gmail.com>
2019-03-05 20:34:57 +0000

    end of book exercise 8 completed

---
+++ exercises/exer8.rb
@@ -0,0 +1,6 @@
+# Ruby's old hash syntax
+p person = { :name => "Winston", :age => 33, :profession => "land surveyor" }
+
+# Ruby's new hash syntax
+
+p person = { name: "Winston", age: 33, profession: "land surveyor" }
