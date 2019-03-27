4d19a836097196a1e8e7b4472d503f8612a96a80
Pablo Vent <ventpdev@gmail.com>
2019-03-27 16:08:32 +0000

    chap 4 exercise 2 completed

--- chap4_flowControl/exer2.rb
+++ chap4_flowControl/exer2.rb
@@ -1,20 +1,6 @@
 def string_converter(string)
-  
-  if string.length > 10
-    string.upcase
-  else
-    string
-  end
+  string.length > 10 ? string.upcase : string
 end
 
 p string_converter("papalapapiricoipi")
 p string_converter("papalapa")
-
-# puts "Type in a word or phrase:"
-#   word_input = gets.chomp.to_s
-
-#   if word_input.length > 10
-#     puts word_input.upcase
-#   else
-#     puts word_input
-#   end
