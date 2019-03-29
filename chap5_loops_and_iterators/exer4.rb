1c7f83f928b5bd502b5e592a51bf82197141b251
Pablo Vent <ventpdev@gmail.com>
2019-03-29 22:35:34 +0000

    chap 5 exercise 4 completed

--- chap5_loops_and_iterators/exer4.rb
+++ chap5_loops_and_iterators/exer4.rb
@@ -1,11 +1,24 @@
-def countdown(start_num)
-  puts start_num
-  if start_num > 0
-    countdown(start_num - 1)
-  end
+# we can use the ternary operator:
+
+def countdown(num)
+  puts num
+  num > 0 ? countdown(num - 1) : return
 end
 
 countdown(12)
-countdown(-28)
+countdown(-18)
+
+
+# or,
+
+# def countdown(start_num)
+#   puts start_num
+#   if start_num > 0
+#     countdown(start_num - 1)
+#   end
+# end
+
+# countdown(12)
+# countdown(-28)
 
 # Although an explicit validation check to see if the input is negative is useful, not including it prints out that negative value anyway. 
