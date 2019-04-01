5b8df5f522614b8f4f35c5e4bf8b1a38616728a5
Pablo Vent <ventpdev@gmail.com>
2019-04-01 23:06:16 +0100

    chap 6 exercise 1 completed.

--- chap6_arrays/exer1.rb
+++ chap6_arrays/exer1.rb
@@ -5,3 +5,23 @@ number = 3
 p arr.include?(number)
 
 # This is the most concise way to check if 3 is in the array.  'p' returns 'true' in this case.
+
+=begin
+# you can also define a method with 2 parameters, which makes the process a bit more involved but also more interesting and dynamic; we asked for input from the user.
+
+puts "Enter the array element you're looking for, please..."
+number = gets.chomp.to_i
+
+arr = [1, 3, 5, 7, 9, 11]
+
+def has_el?(arr, number)
+  if arr.include?(number)
+    puts "The element is within the array!"
+  else
+    puts "Not here buddy!"
+  end
+end
+
+has_el?(arr, number)
+
+=end
